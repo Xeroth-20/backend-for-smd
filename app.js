a9f8e1cb01a47240b0d5f217139ab9d41dbbca69
Jhonatan Castillo <jhonatancastillox@gmail.com>
2020-09-22 18:52:20 -0500

    1.0.0

---
+++ app.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const app = express();
+
+/* random number function */
+const getRandomNumber = (min = 150, max = 99998) => {
+    return min > max ? Math.floor(Math.random() * min) + 1 : Math.floor(min + (Math.random() * (max - min))) + 1;
+}
+
+const getPercentFromRandomNumber = (min = -45555) => {
+    return getRandomNumber(min) / 100;
+}
+
+const generateSnUser = () => {
+    const snUser = {
+        firstname: 'Jhonatan',
+        lastname: 'Castillo',
+        email: 'jhonatancastillox@gmail.com',
+        totalFollowers: 0,
+        stats: {
+            facebook: {
+                snUsername: 'JhonatanCastillo',
+                totalFollowers: getRandomNumber(),
+                todayFollowers: getRandomNumber(-5555, 6666),
+                overview: {
+                    pageViews: {
+                        today: getRandomNumber(),
+                        increase: getPercentFromRandomNumber(),
+                    },
+                    pageLikes: {
+                        today: getRandomNumber(),
+                        increase: getPercentFromRandomNumber()
+                    }
+                }
+            },
+            twitter: {
+                snUsername: 'JCowo',
+                totalFollowers: getRandomNumber(),
+                todayFollowers: getRandomNumber(-5555, 6666),
+                overview: {
+                    likes: {
+                        today: getRandomNumber(),
+                        increase: getPercentFromRandomNumber(),
+                    },
+                    retweets: {
+                        today: getRandomNumber(),
+                        increase: getPercentFromRandomNumber()
+                    }
+                }
+            },
+            instagram: {
+                snUsername: 'JC',
+                totalFollowers: getRandomNumber(),
+                todayFollowers: getRandomNumber(-5555, 6666),
+                overview: {
+                    likes: {
+                        today: getRandomNumber(),
+                        increase: getPercentFromRandomNumber(),
+                    },
+                    profileViews: {
+                        today: getRandomNumber(),
+                        increase: getPercentFromRandomNumber()
+                    }
+                }
+            },
+            youtube: {
+                snUsername: 'FriedAvocado',
+                totalFollowers: getRandomNumber(),
+                todayFollowers: getRandomNumber(-5555, 6666),
+                overview: {
+                    likes: {
+                        today: getRandomNumber(),
+                        increase: getPercentFromRandomNumber(),
+                    },
+                    views: {
+                        today: getRandomNumber(),
+                        increase: getPercentFromRandomNumber()
+                    }
+                }
+            }
+        }
+    }
+
+    for (let sn in snUser.stats) {
+        snUser.totalFollowers += snUser.stats[sn].totalFollowers;
+    }
+
+    return snUser;
+}
+
+/* cors */
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+})
+
+/* port */
+app.set('port', 8000);
+
+/* routes */
+app.get('/api/v1/user', async (req, res) => {
+    res.json(generateSnUser());
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
